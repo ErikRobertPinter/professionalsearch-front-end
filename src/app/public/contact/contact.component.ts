6141451bc167d5e415ed95f9f333025e33aae34f
Erik2013-P <erikpinter15@gmail.com>
2025-05-06 12:04:50 +0200

    new authentication forms partly added

--- src/app/public/contact/contact.component.ts
+++ src/app/public/contact/contact.component.ts
@@ -1,11 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
-
+  contact = new FormGroup({
+    surname: new FormControl(''),
+    firstname: new FormControl(''),
+    email: new FormControl(''),
+    phoneNumber: new FormControl(''),
+    subject: new FormControl(''),
+    message: new FormControl(''),
+  });
 }
--- src/app/public/forgot-password/forgot-password.component.ts
+++ src/app/public/forgot-password/forgot-password.component.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './forgot-password.component.html',
-  styleUrl: './forgot-password.component.scss'
+  styleUrl: './forgot-password.component.scss',
 })
 export class ForgotPasswordComponent {
-
+  forgotPassWordForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+  onSubmit() {}
 }
